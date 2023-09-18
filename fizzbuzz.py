fd9a937dbd62b7c2346269197537ddc8328b33bd
alex50027319 <ruddms7319@ajou.ac.kr>
2023-09-18 18:31:38 +0900

    feat : Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,7 @@
-for i in range(1, 101):   
-    if i % 3 == 0 and i % 5 == 0:    
-        print('FizzBuzz')            
-    elif i % 3 == 0:                 
-        print('Fizz')                
-    elif i % 5 == 0:                 
-        print('Buzz')                
-    else:
-        print(i)  
+n = int(input("input max number"))
+
+fizz_list = ['FizzBuzz' if i % 3 == 0 and i % 5 == 0 else 'Fizz' if i % 3 == 0 else 'Buzz' if i % 5 == 0 else i for i in range(1, n)]
+
+print(fizz_list)
+
+
